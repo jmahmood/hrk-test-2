d774d0c1c966f8f3a7615f31ebbee6796682a6ec
Jawaad Mahmood <jawaad.mahmood@gmail.com>
2017-07-01 13:09:23 +0900

    send hello world to hrk-test's hello world logging tool

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
+import requests
 
 def index(request):
     logging.warning('Hello World executed on warm-forest-44218')
+    requests.post("https://still-basin-26156.herokuapp.com/logs", "Hello World executed on warm-forest-44218")
     return HttpResponse('Hello World')
