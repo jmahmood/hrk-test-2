7d9dc334ca9a121e583ce9bcc1adef8d3ee4a4cd
Jawaad Mahmood <jawaad.mahmood@gmail.com>
2017-07-01 12:17:18 +0900

    Added hello world

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 import hello.views
 
 # Examples:
@@ -11,6 +8,4 @@ import hello.views
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import logging
 
-from .models import Birthday
-
 def index(request):
     return HttpResponse('Hello World')
