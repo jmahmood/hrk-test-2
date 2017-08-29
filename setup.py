f5a56df62a26574f983b88d887e015acd641da84
Jawaad Mahmood <ideas@jawaadmahmood.com>
2017-08-29 11:15:03 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    install_requires=[
+        'django-wizard-builder==1.0.1',
+    ],
+)
